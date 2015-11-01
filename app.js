73f339ae62bbfd7695ab01a0d4a6291e59a532db
madhudskumar <madhudskumar>
2015-11-01 20:17:20 +0530

    0.0.1

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var book = require('./models/bookModel');
+
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get(function(req,res){
+        book.find(function(err,book){
+            if(err) {
+                //console.log(err);//not to implement
+                res.status(500).send(err);
+            }
+            else{
+                res.json(book);
+            }
+        });
+        //res.json(book); // resend a json object
+    });
+
+app.use('/api', bookRouter); //  to use a specifick get / post router
 
 app.get('/',function(req,res){
     res.send("hello welcome to api");
