918948da8ca345ace54c9540d83470bb08e8d9bb
madhudskumar <madhudskumar>
2015-11-01 23:34:47 +0530

    0.0.2

--- app.js
+++ app.js
@@ -9,7 +9,16 @@ var bookRouter = express.Router();
 
 bookRouter.route('/books')
     .get(function(req,res){
-        book.find(function(err,book){
+
+        var query = {};
+
+        query = req.query;  // ?genre = x === req.query = {genre : fiction}
+
+        if(req.query.genre){
+            query.genre = req.query.genre;   ///to filter dataabase : ;
+        }
+
+        book.find(query ,function(err,book){
             if(err) {
                 //console.log(err);//not to implement
                 res.status(500).send(err);
@@ -19,6 +28,20 @@ bookRouter.route('/books')
             }
         });
         //res.json(book); // resend a json object
+    });
+
+bookRouter.route('/books/:id')
+    .get(function(req,res){
+
+        book.findById(req.params.id ,function(err,book){
+            if(err) {
+                //console.log(err);//not to implement
+                res.status(500).send(err);
+            }
+            else{
+                res.json(book);
+            }
+        });
     });
 
 app.use('/api', bookRouter); //  to use a specifick get / post router
